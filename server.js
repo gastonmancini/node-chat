1ca66495427114f9d153b8199702f0451b123712
Gaston Mancini <gmancini@makingsense.com>
2015-06-01 17:08:04 -0300

    Integrating express.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var mime = require('mime');
-var cache = {};
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
 
 /**
  * 404 response
@@ -22,59 +20,19 @@ function send500(response) {
 	response.end();
 }
 
-/**
- * Retrieve a specific file
- */
-function sendFile(response, filePath, fileContents) {
-	response.writeHead(
-	    200, 
-	    {"content-type": mime.lookup(path.basename(filePath))}
-	);
-	response.end(fileContents);
-}
+app.use(express.static(__dirname + '/public'));
 
-/**
- * Serve a static file - from cache or disk
- */
-function serveStatic(response, cache, absPath) {
-	if (cache[absPath]) {
-		sendFile(response, absPath, cache[absPath]);
-	} else {
-		fs.exists(absPath, function(exists) {
-			if (exists) {
-				fs.readFile(absPath, function(err, data) {
-					if (err) {
-						send500(response);		
-					} else {
-						cache[absPath] = data;	
-						sendFile(response, absPath, data);
-					}
-				});
-			} else {
-				send404(response);
-			}
-		});
-	}
-}
-
-/**
- * Create server
- */
-var server = http.createServer(function(request, response) {
-	var filePath = false;
-	
-	if (request.url == '/') {
-		filePath = 'public/index.html';
-	} else {
-		filePath = 'public' + request.url;
-	}
-	
-	var absPath = './' + filePath;
-	serveStatic(response, cache, absPath);
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', server.address().port);
+/*
+app.get('/archive', function(req, res){
+	console.log("test");
+});*/
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on *:3000');
 });
 
 /**
@@ -82,5 +40,5 @@ server.listen(process.env.PORT || 3000, function(){
  * Starts the socket.io server functionality (provide  the server so it can share the same tcp/ip port)
  */
 var chatServer = require('./lib/chat_server');
-chatServer.listen(server);
+chatServer.listen(http);
 
