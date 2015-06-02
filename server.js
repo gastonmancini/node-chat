be548f961f4550d939a60471fc11da95697c0a0c
Gaston Mancini <gmancini@makingsense.com>
2015-06-02 14:12:30 -0300

    Changing some comments

--- server.js
+++ server.js
@@ -4,33 +4,22 @@ var http = require('http').Server(app);
 var common = require('./lib/foundation/common');
 var chatService = require('./lib/services/chatService');
 
-/**
- * Serve static assets
- */
+// Serve static assets
 app.use(express.static(__dirname + '/public'));
 
-/**
- * Set views and template engine
- */
+// Set views and template engine
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-/**
- * Chat endpoint
- */
+// Chat Main UI
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-/**
- * Chat history for room View (render html)
- */
+// Chatroom history
 app.get('/history/:room', chatService.list);
 
-
-/**
- * Chat history for room API (returns json)
- */
+// API chatroom histpry endpoints
 app.get('/api/history/:room', chatService.listAsJson);
 
 http.listen(common.listenPort(), function () {
