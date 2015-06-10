49d8c65f0d53d58bc0ed393b22a94b3e9e379f25
Gaston Mancini <gmancini@makingsense.com>
2015-06-10 17:28:01 -0300

    Install gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var paths = {
+    scripts: ['gulpfile.js', 'public/javascripts/**/*.js']
+};
+
+gulp.task('lint', function () {
+    return gulp.src(paths.scripts)
+        .pipe(jshint( { 
+						"globals": {
+						    "angular": false,
+							"io": false,
+							"strinct": true
+						} 
+					   }))
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch(paths.scripts, ['lint']);
+});
+
+gulp.task('default', ['watch']);
