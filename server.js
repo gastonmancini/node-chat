cc89084d11df5ee3d38789b1c0e9343c67582370
Gaston Mancini <gmancini@makingsense.com>
2015-06-11 17:14:56 -0300

    Move routes to a different controllers and call it from the bootstrap module

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var config = require('./lib/config/config');
-var mongo = require('./lib/models/bootstrap');
-var chatService = require('./lib/services/chatService');
-var userService = require('./lib/services/userService');
 var passport = require('passport');
 var cookieParser = require('cookie-parser');
 var expressSession = require('express-session');
@@ -13,7 +10,6 @@ var expressSession = require('express-session');
 // Configure express
 app.use(express.static(__dirname + '/public'));
 app.use('/lib', express.static(__dirname + '/node_modules/'));
-
 app.use(cookieParser());
 app.use(expressSession({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true }));
 
@@ -25,18 +21,8 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
-var auth = require('./lib/foundation/auth');
-
-// API auth
-app.post('/users', userService.registerUser);
-app.get('/auth', auth.ensureAuthenticated, auth.session);
-app.post('/auth', auth.login);
-app.delete('/auth', auth.logout);
+var bootstrapNode = require('./lib/bootstrap')(app);
   
-// API chatroom history endpoints
-app.get('/api/history/:room', auth.ensureAuthenticated, chatService.getChatLines);
-app.get('/api/chatroom', auth.ensureAuthenticated, chatService.getChatRooms);
-
 http.listen(config.port, function () {
 	console.log('Listening port:' + config.port);
 });
