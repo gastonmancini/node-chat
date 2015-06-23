72bcb04e6700121f5c2f4cc1376b8d4b14b5869c
Gaston Mancini <gmancini@makingsense.com>
2015-06-23 14:23:05 -0300

    Use a restricted variant of javascript (include use-strict on the backend)

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var http = require('http').Server(app);
-var morgan = require('morgan');
+(function(){
+    
+	'use strict';
+	
+	var express = require('express');
+	var bodyParser = require('body-parser');
+	var app = express();
+	var http = require('http').Server(app);
+	var morgan = require('morgan');
+	
+	// Configure express
+	app.use(express.static(__dirname + '/public'));
+	app.use('/lib', express.static(__dirname + '/node_modules/'));
+	
+	// Middleware to parse the JSON body configure app to use bodyParser() this will let us get the data from a POST
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+	
+	// Use morgan to log requests to the console
+	app.use(morgan('dev'));
+	
+	// Bootstrap application
+	var nodechat = require('./lib/core/bootstrap')(app);
+	  
+	http.listen(nodechat.config.port, function () {
+		console.log('Listening port:' + nodechat.config.port);
+	});
+	
+	/**
+	 * Load custom Node functionality, and
+	 * Starts the socket.io server functionality (provide  the server so it can share the same tcp/ip port)
+	 */
+	nodechat.applicationServices.chatServer.listen(http);
 
-// Configure express
-app.use(express.static(__dirname + '/public'));
-app.use('/lib', express.static(__dirname + '/node_modules/'));
-
-// Middleware to parse the JSON body configure app to use bodyParser() this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// Use morgan to log requests to the console
-app.use(morgan('dev'));
-
-// Bootstrap application
-var nodechat = require('./lib/core/bootstrap')(app);
-  
-http.listen(nodechat.config.port, function () {
-	console.log('Listening port:' + nodechat.config.port);
-});
-
-/**
- * Load custom Node functionality, and
- * Starts the socket.io server functionality (provide  the server so it can share the same tcp/ip port)
- */
-nodechat.applicationServices.chatServer.listen(http);
+})();
